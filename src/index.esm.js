fd7a5556a8be35fa5b7733676e21f5065b75d540
iansun <lauer3912@gmail.com>
2017-03-19 16:33:22 +0800

    Optimize for browser

--- src/index.esm.js
+++ src/index.esm.js
@@ -1,17 +1,24 @@
+import { Observable, SelfClass } from './dove.sdk/observable'
+
 import b$ from './dove.sdk/bs/index'
 import underscore from './dove.sdk/underscore'
-import { Observable, SelfClass } from './dove.sdk/observable'
 import util from './dove.sdk/util/index'
 
-window.BS = b$
-window.Romanysoft = {
-  _: underscore._,
-  Util: util,
-  Observable: Observable,
-  SelfClass: SelfClass,
-  BS: b$
+try {
+  if (window) {
+    window.BS = b$
+    window.Romanysoft = {
+      _: underscore._,
+      Util: util,
+      Observable: Observable,
+      SelfClass: SelfClass,
+      BS: b$
+    }
+    window.DoveMax = window.Romanysoft
+  }
+} catch (error) {
+  console.warn(error)
 }
-window.DoveMax = window.Romanysoft
 
 export default {
   _: underscore._,
