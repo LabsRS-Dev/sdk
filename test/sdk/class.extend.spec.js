45fb178f2615fdec87b175ce53d53f6c0d48e398
iansun <lauer3912@gmail.com>
2018-01-05 16:27:45 +0800

    backup

--- test/sdk/class.extend.spec.js
+++ test/sdk/class.extend.spec.js
@@ -1,4 +1,5 @@
 import { Observable, SelfClass } from '../../src/dove.sdk/observable'
+import _ from 'lodash'
 
 describe('SDK.Class', () => {
   it('Class: extend issues es not this', () => {
@@ -64,5 +65,55 @@ describe('SDK.Observable', () => {
       expect(msgObj.data).toEqual(undefinedV)
     }, false)
     mc.trigger('helloUndefined', undefinedV)
+  })
+
+  it('Observable: Test each register and unregister method ..', () => {
+    const mc = new Observable()
+
+    var __$p$ = {
+      mc: mc
+    }
+
+    const __msgPrefix = ''
+    const TypeMsg = {
+      OnCreateError: __msgPrefix + 'OnCreateError', // 创建失败
+      OnRunning: __msgPrefix + 'OnRunning', // 创建并连接上
+    
+      OnGetServerMessage: __msgPrefix + 'OnGetServerMessage', // 从服务器获取到信息
+      OnSendMessageToServer: __msgPrefix + 'OnSendMessageToServer' // 向服务器发送信息
+    }
+
+    // 批量处理注册及接收方式
+    _.each(TypeMsg, function (eventType, key, list) {
+      var registerKey = 'register' + key
+      var unregisterKey = 'unregister' + key
+
+      __$p$[registerKey] = function (handler, one = false) {
+        __$p$.mc.bind(eventType, handler, one)
+      }
+      __$p$[unregisterKey] = function (handler) {
+        __$p$.mc.unbind(eventType, handler)
+      }
+    })
+
+    console.log("mc = ")
+    console.dir(__$p$.mc)
+
+    console.log("__$p$ = ")
+    console.dir(__$p$)
+
+    expect(_.isFunction(__$p$['registerOnCreateError'])).toEqual(true)
+    expect(_.isFunction(__$p$['registerOnCreateError2'])).toEqual(false)
+
+    const msg = 'Hello'
+    __$p$.registerOnCreateError(function(msgObj){
+      console.log("[1]registerOnCreateError ...")
+      expect(msgObj.data).toEqual(msg)
+    })
+    __$p$.registerOnSendMessageToServer(function(msgObj){
+      console.log("[2]registerOnSendMessageToServer: ...")
+      expect(msgObj.data).toEqual(msg)
+    })
+    __$p$.mc.trigger(TypeMsg.OnCreateError, msg)
   })
 })
